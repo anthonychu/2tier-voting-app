e393f95902892517d41b9be0c063bd537dc607f5
Anthony Chu <antchu@microsoft.com>
2018-04-23 12:11:25 -0700

    Add API app

---
+++ src/Voting.Api/Controllers/VotesController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Voting.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class VotesController : Controller
+    {
+        private static VotingResults results = new VotingResults();
+        private static object lockObject = new object();
+        
+        [HttpGet]
+        public VotingResults Get()
+        {
+            return results;
+        }
+
+        [HttpPost]
+        public VotingResults Post([FromQuery]string vote)
+        {
+            lock (lockObject)
+            {
+                if (vote == "cats")
+                {
+                    results.Cats += 1;
+                }
+                else if (vote == "dogs")
+                {
+                    results.Dogs += 1;
+                }
+            }
+            return results;
+        }
+        
+        [HttpDelete]
+        public VotingResults Delete()
+        {
+            lock (lockObject)
+            {
+                results.Dogs = results.Cats = 0;
+            }
+            return results;
+        }
+    }
+
+    public class VotingResults
+    {
+        public int Cats { get; set; } = 0;
+        public int Dogs { get; set; } = 0;
+    }
+}
